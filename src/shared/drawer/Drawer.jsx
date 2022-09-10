fb362a2b8d9097831ffce03a1be20bd9a8fbc26d
Mohamed Saleh <mohamedsaleh181@gmail.com>
2022-09-10 17:05:55 +0200

    some enhance of code and fix some bugs

--- src/shared/drawer/Drawer.jsx
+++ src/shared/drawer/Drawer.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 
+
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
